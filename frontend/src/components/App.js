4870b26d39ba849bbf5bcb530a081fe49fbf2191
Den <k1selevde@yandex.ru>
2020-12-03 21:04:38 +0500

    add react with babel, webpack, static not work

---
+++ frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Todo wqewqeqwe app</h1>
+                <p>What is it?? ?? ?</p>
+            </div>
+        )
+    }
+}
+
+export default App;
+
+
